ee704fca8a5ceebc90ab3ae40ca89249c71e03f4
bcherek <144633921+bcherek@users.noreply.github.com>
2025-07-08 19:40:03 +0200

    up to date

--- src/components/handmade/top-header.tsx
+++ src/components/handmade/top-header.tsx
@@ -1,11 +1,15 @@
 
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
+const basePath = publicRuntimeConfig?.basePath || '';
+// 
 
 export function TopHeader() {
     return (
     <div className="flex flex-col justify-center align-middle bg-uchicago-red p-4">
         <div className="font-bold text-white text-center text-3xl">Levi Cherek</div>
         <div className="flex flex-row justify-center space-x-7 align-middle text-white">
-            <a href="/resume.pdf" download="instructions.pdf" className="underline">Resume</a>
+            <a href={`${basePath}/resume.pdf`} download="instructions.pdf" className="underline">Resume</a>
             <a href="https://www.linkedin.com/in/levicherek/" className="underline">LinkedIn</a>
             <a href="https://ch.tetr.io/u/levi" className="underline">TETR.IO</a>
         </div>
